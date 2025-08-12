cf8b8207990d775e291ffed82f64353b0a32ad7a
zinmoe <zinawsglory@gmail.com>
2025-08-12 22:07:05 +0400

    add ec2 terraform

---
+++ ec2.tf
@@ -0,0 +1,37 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.ssh_sg.id]
+  key_name                    = aws_key_pair.generated.key_name
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "HelloWorld"
+  }
+
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.ssh_key.private_key_pem}' > private_key.pem && chmod 400 private_key.pem"
+  }
+}
